8165db6f5a577af131e7036e45c5c8c476604d98
Danil Nikiforov <danil.nikiphorov@gmail.com>
2023-09-05 12:55:17 +0500

    change labels and update logic to get first move to ready for demo column
    It's sad that trello limit board history and I can no longer get full history of cards moving

--- get_tasks.py
+++ get_tasks.py
@@ -15,14 +15,14 @@ def extract_tasks_from_trello(json_file):
         if 'infra' in labels:
             continue
 
-        task = {}
-        task['name'] = card['name']
-        task['description'] = card['desc']
-        task['labels'] = [label['name'] for label in card['labels']]
-        task['due_date'] = card['due'] if 'due' in card else None
-        task['short_url'] = card['shortUrl']
-        task['closed'] = card['closed']
-        
+        if 'Frontend' in labels:
+            continue
+
+        task = {'name': card['name'], 'description': card['desc'],
+                'labels': [label['name'] for label in card['labels']],
+                'due_date': card['due'] if 'due' in card else None, 'short_url': card['shortUrl'],
+                'closed': card['closed'], 'complete_date':'', 'estimate':0}
+
         # Check if list ID matches and set 'done' field accordingly
         if check_if_done(card['idList']):
             task['done'] = True
@@ -37,25 +37,47 @@ def extract_tasks_from_trello(json_file):
             task['estimate'] = None
 
         # Find the first action where the task was moved to the desired list
+
         for action in data['actions']:
             if action['type'] == 'updateCard' and action['data']['card']['id'] == card['id']:
                 if 'listAfter' in action['data']:
-                    if check_if_done(action['data']['listAfter']['id']):
+                    print(task['name'], action['data']['listAfter']['name'], datetime.strptime(action['date'], "%Y-%m-%dT%H:%M:%S.%fZ"))
+                    print("/n")
+                    if check_if_ready_for_demo(action['data']['listAfter']['id']):
                         move_date = datetime.strptime(action['date'], "%Y-%m-%dT%H:%M:%S.%fZ")
                         task['complete_date'] = move_date.strftime("%Y-%m-%d %H:%M:%S")
-                        break
-        else:
-            task['complete_date'] = None
+                        print('--')
+                        print(task['name'], task['complete_date'])
+                        print("--")
+                        # break
+
+        if task['complete_date'] == '':
+            for action in data['actions']:
+                if action['type'] == 'updateCard' and action['data']['card']['id'] == card['id']:
+                    if 'listAfter' in action['data']:
+                        if check_if_done(action['data']['listAfter']['id']):
+                            move_date = datetime.strptime(action['date'], "%Y-%m-%dT%H:%M:%S.%fZ")
+                            task['complete_date'] = move_date.strftime("%Y-%m-%d %H:%M:%S")
+                            print('--')
+                            print(task['name'], task['complete_date'])
+                            print("--")
+                            # break
 
         tasks.append(task)
 
     return tasks
 
-def check_if_done(listId):
+def check_if_ready_for_demo(listId):
     listDoneId1 = '642c5aedd95db65295aa3200' #готово к демо
+    # listDoneId2 = '642c5aedd95db65295aa3201' #Done
+
+    return listId == listDoneId1
+
+def check_if_done(listId):
+    # listDoneId1 = '642c5aedd95db65295aa3200' #готово к демо
     listDoneId2 = '642c5aedd95db65295aa3201' #Done
 
-    return listId == listDoneId1 or listId == listDoneId2
+    return listId == listDoneId2
 
 def save_tasks_to_csv(tasks, csv_file):
     fieldnames = ['Task Name', 'URL', 'Archived', 'Complete Date', 'Estimate']
